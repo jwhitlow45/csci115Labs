a4a9034a9b2f2abe837a6f91eee6be544e20ee11
jwhitlow45 <jwhitlow45@gmail.com>
2020-12-13 12:30:42 -0800

    Finished dijkstra

--- lab13-dijkstra/lab13 - dijkstra.cpp
+++ lab13-dijkstra/lab13 - dijkstra.cpp
@@ -6,165 +6,59 @@
 
 using namespace std;
 
-#define INT_MAX 2147483647
-
-class Node
-{
-private:
-	int node;
-	int weight;
-	Node* next = nullptr;
-	void setNode(int arg) { node = arg; }
-public:
-	void setWeight(int arg) { weight = arg; }
-	void setNext(Node* arg) { next = arg; }
-	int getNode() { return node; }
-	int getWeight() { return weight; }
-	Node* getNext() { return next; }
-
-	Node(int argNode, int argWeight)
-	{
-		setNode(argNode);
-		setWeight(argWeight);
-	}
-};
-
 class Graph
 {
 private:
 	int size;
 	void setSize(int argSize) { size = argSize; }
 public:
-	Node** matrix;
+	int** matrix;
 	int getSize() { return size; }
 
-	void addEdge(int u, int v, int weight)
-	{	//add edge to adj graph
-		if (matrix[u] == nullptr)
-		{
-			matrix[u] = new Node(v, weight);
-			return;
-		}
-		Node* cur = matrix[u];
-		while (cur->getNext() != nullptr)
-			cur = cur->getNext();
-		cur->setNext(new Node(v, weight));
+	//set edge of graph
+	void setEdge(int u, int v, int weight)
+	{
+		matrix[u][v] = weight;
 	}
 
-	int getEdgeWeight(int u, int v)
-	{	//get weight from edge u to v
-		Node* cur = matrix[u];
-		while (cur != nullptr && cur->getNode() != v)
-			cur = cur->getNext();
-		if (cur == nullptr)
-			return -1;
-		return cur->getWeight();
-	}
-
+	//print graph matrix
 	void print()
-	{	//display graph
+	{
 		for (size_t i = 0; i < getSize(); i++)
 		{
-			Node* cur = matrix[i];
-			cout << i << ": ";
-			while (cur != nullptr)
-			{
-				cout << cur->getNode() << "," << cur->getWeight() << " -> ";
-				cur = cur->getNext();
-			}
+			for (size_t j = 0; j < getSize(); j++)
+				cout << matrix[i][j] << '\t';
 			cout << endl;
 		}
 	}
 
-	Graph(int argSize)
-	{
-		setSize(argSize);
-		matrix = new Node* [getSize()];
-		for (size_t i = 0; i < getSize(); i++)
-			matrix[i] = nullptr;
-	}
+	//release memory used by object
 	void cleanup()
 	{
 		for (size_t i = 0; i < getSize(); i++)
-		{
-			Node* cur = matrix[i];
-			Node* next = cur;
-			while (cur != nullptr)
-			{
-				next = cur->getNext();
-				delete cur;
-				cur = next;
-			}
-		}
+			delete[] matrix[i];
 		delete[] matrix;
 	}
-};
 
-class minPQ
-{
-private:
-	vector<Node> q;
-public:
-	Node top()
-	{	//get top value from queue
-		return q.front();
-	}
-	void pop()
-	{	//remove top value from queue
-		q.erase(q.begin());
-	}
-	void push(Node myNode)
+	Graph(int argSize)
 	{
-		//push new value to back of queue and sort into queue
-		q.push_back(myNode);
-		int cur = q.size() - 1;
-		while (cur > 0 && q[cur].getWeight() < q[cur - 1].getWeight())
+		setSize(argSize);	//set size of graph
+
+		//init matrix to 0
+		matrix = new int* [getSize()];
+		for (size_t i = 0; i < getSize(); i++)
 		{
-			swap(q[cur], q[cur - 1]);
-			cur--;
+			matrix[i] = new int[getSize()];
+			for (size_t j = 0; j < getSize(); j++)
+				matrix[i][j] = 0;
 		}
-	}
-	bool decKey(int key, int newWeight)
-	{
-		int index = find(key);	//find key in queue
-		if (index == -1)		//if not in queue return false
-		{
-			cout << "ERROR:Not in queue\n";
-			return false;
-		}
-		if (newWeight > q[index].getWeight())	//if new weight too large
-		{										//return false
-			cout << "ERROR:New weight larger than old weight\n";
-			return false;
-		}
-		q[index].setWeight(newWeight);
-		while (index > 0 && q[index].getWeight() < q[index - 1].getWeight())
-		{
-			swap(q[index], q[index - 1]);
-			index--;
-		}
-		return true;
-	}
-	bool empty()
-	{
-		return !q.size();
-	}
-	int find(int key)
-	{
-		//iterate through queue looking for value
-			//return the index if found
-			//return -1 if not found
-		for (size_t i = 0; i < q.size(); i++)
-		{
-			if (q[key].getNode() == key)
-				return i;
-		}
-		return -1;
 	}
 };
 
 //function prototypes
 void dijkstra(Graph G, int start);
+int findClosestNode(Graph G, int distance[], bool visited[]);
+void printSolution(Graph G, int distance[]);
 
 int main()
 {
@@ -178,9 +72,11 @@ int main()
 	cout << "Enter edges in format u,v,weight;u,v,weight;...;u,v,weight:";
 
 	//build graph
-	Graph myGraph(size);
 	string input;
 	getline(cin, input);
+
+	Graph myGraph(size);
+
 	stringstream inputs(input);
 	while (inputs.good())
 	{
@@ -191,58 +87,76 @@ int main()
 		getline(inputs, v, ',');
 		getline(inputs, weight, ';');
 
-		myGraph.addEdge(stoi(u), stoi(v), stoi(weight));
+		myGraph.setEdge(stoi(u), stoi(v), stoi(weight));
 	}
+	cout << "\nGraph\n";
 	myGraph.print();
 
 	cout << endl;
 	dijkstra(myGraph, start);
+	return 0;
+}
+
+int findClosestNode(Graph G, int distance[], bool visited[])
+{
+	int min = INT_MAX;
+	int index;
+	for (size_t i = 0; i < G.getSize(); i++)
+	{
+		if(!visited[i] && distance[i] <= min)
+		{
+			min = distance[i];
+			index = i;
+		}
+	}
+	return index;
 }
 
 void dijkstra(Graph G, int start)
 {
-	int* estArr = new int[G.getSize()];
-	//init array to inf
-	for (size_t i = 0; i < G.getSize(); i++)
-		estArr[i] = INT_MAX;
-	
-	//set starting point
-	estArr[start] = 0;
+	int* distance = new int[G.getSize()];	//keep track of distance from i
+	bool* visited = new bool[G.getSize()];	//keep track of visited nodes
 
-	minPQ pq;
 	for (size_t i = 0; i < G.getSize(); i++)
-		pq.push(Node(i, estArr[i]));
+	{	//set all distances to inf, all nodes to not visited
+		distance[i] = INT_MAX;
+		visited[i] = false;
+	}
 
-	//shortest path set
-	vector<Node> S;
-	while (!pq.empty())
+	//set distance of starting node to 0 (so we know where to start)
+	distance[start] = 0;
+
+	//relax edge |V|-1 times
+	for (size_t i = 0; i < G.getSize()-1; i++)
 	{
-		//extract min
-		Node u = pq.top();
-		pq.pop();
-		
-		//add u to shortest path set S
-		S.push_back(u);
-
-		Node* v = G.matrix[u.getNode()];	//get adj matrix of u
-		while (v != nullptr)
+		int u = findClosestNode(G, distance, visited);	//find next closest node
+		visited[u] = true;	//mark u as visited
+		for (size_t v = 0; v < G.getSize(); v++)
 		{
-			int newWeight = estArr[u.getNode()] +
-				G.getEdgeWeight(u.getNode(), v->getNode());
-			if (estArr[v->getNode()] > newWeight);
+			//update distance[i] if not visited and a shorter path is found
+			if (!visited[v] &&				//not visited
+				G.matrix[u][v] &&			//a connection exists
+				distance[u] != INT_MAX &&	//distance is not inf
+				distance[u] + G.matrix[u][v] < distance[v])	//shorter path
 			{
-				estArr[v->getNode()] = newWeight;
-				pq.decKey(v->getNode(), newWeight);
+				distance[v] = distance[u] + G.matrix[u][v];
 			}
-			v = v->getNext();
 		}
 	}
-	
-	cout << "Source\t\t" << "Distance from root";
+
+	printSolution(G, distance);
+}
+
+void printSolution(Graph G, int distance[])
+{
+	cout << "Source\tDistance\n";
 	for (size_t i = 0; i < G.getSize(); i++)
 	{
-		cout << i << "\t\t" << estArr[i];
+		if (distance[i] == INT_MAX)
+			cout << i << "\tCannot reach from source!\n";
+		else if (distance[i] == 0)
+			cout << i << "\tStart\n";
+		else
+			cout << i << '\t' << distance[i] << endl;
 	}
-
-	delete[] estArr;
 }
